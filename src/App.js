8f9aec8d0228424d6889a4503b48571316cbe335
Marshall <marshallstutzman@gmail.com>
2021-01-29 11:37:25 -0500

    react-helmet test & 'global' variables test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
+// Helmet test
+import Site from "./_site_var"
+import { Helmet } from "react-helmet";
+
 const App = () => {
   return (
 	<>
+	  <Helmet>
+		<title>{ Site.name } | Title</title>
+	  	<meta name="description" content="This is a test description" />
+	  </Helmet>
 	  <Header>
 		<Nav />
 	  </Header>
-	  
+	   <h3 style={{ fontSize: "50px" }}> {Site.location} </h3>
 	  <Footer />
 	</>
   );
---
+++ src/_site_var.js
@@ -0,0 +1,8 @@
+const Site = {
+	name: "My Site",
+	location: "100 Test st.",
+	phone: "(574) 555-5555",
+	email: "site_email@email.com"
+}
+
+export default Site;
