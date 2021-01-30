6859159d823f3627bbb52613525d5d3dc83bd6c4
wowTerrific <wowterrific@localhost.localdomain>
2021-01-30 13:36:12 -0500

    set up mobile navigation toggle - still needs onClick function for line items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,31 +4,39 @@ import { useState } from 'react';
 
 const Nav = () => {
 
-	const [showMobile, setShowMobile] = useState(false)
-
-
-// look up ReactJS useEffect to handle resize
-// check view-width to see if <= 767px
-// if true - show mobile nav component
-// else show current
-
+	const [showMobile, setShowMobile] = useState(false);
+	const [navHide, setNavHide] = useState('hide-nav')
 
 
 	return (
 	    <div id="nav-container">
-	        <div id="nav-btn-mobile" onClick={ () => {setShowMobile(!showMobile); console.log(showMobile)} }>
+	        <div id="nav-btn-mobile" onClick={ () => {
+	          setShowMobile(!showMobile);
+	          setNavHide( navHide === 'hide-nav' ? '' : 'hide-nav')
+	          console.log(showMobile)} 
+	          }>
 	            <div className="nav-btn-bar"></div>
 	            <div className="nav-btn-bar"></div>
 	            <div className="nav-btn-bar"></div>
 	        </div>
-		    <nav>
-			    <ul>
-				    <li><Link to="/">Home</Link></li>
-				    <li><Link to="/services">Services</Link></li>
-				    <li><Link to="/about">About</Link></li>
-				    <li><Link to="/contact">Contact</Link></li>
-			    </ul>
-		    </nav>
+	        
+						<nav id="desk-nav">
+							<ul>
+								<li><Link to="/">Home</Link></li>
+								<li><Link to="/services">Services</Link></li>
+								<li><Link to="/about">About</Link></li>
+								<li><Link to="/contact">Contact</Link></li>
+							</ul>
+						</nav>
+						
+						<nav id="mobile-nav" className={ navHide }>
+							<ul>
+								<Link to="/"><li>Home</li></Link>
+								<Link to="/services"><li>Services</li></Link>
+								<Link to="/about"><li>About</li></Link>
+								<Link to="/contact"><li>Contact</li></Link>
+							</ul>
+						</nav>
 		</div>
 	)
 }
