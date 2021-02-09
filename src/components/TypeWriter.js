0906f99fdac8887185feed33b21ca4b52ef9d873
wowTerrific <marshallstutzman@gmail.com>
2021-02-08 20:02:22 -0500

    Working on typewriter loop

--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -1,13 +1,28 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TypeWriter = ( props ) => {
 
 	const initArray = props.write;
 
-	const [ writeArray, setWriteArray ] = useState('');
+	const [ writeArray, setWriteArray ] = useState(initArray[0]);
 	const [ animName, setAnimName ] = useState('typewrite');
+	const [ arrayIndex, setArrayIndex ] = useState(0);
 
+	useEffect( () => {
+		setTimeout( () => {
+			changeWrite();
+		}, 3000);
+	}, [writeArray]);
+
+	const changeWrite = () => {
+		console.log(arrayIndex);
+	  if (arrayIndex < initArray.length) {
+		 setArrayIndex(arrayIndex + 1) 
+		 setWriteArray(initArray[arrayIndex]);
+	  }
+	};
+	
 	// setWriteArray
 	// pause
 	// set anim name to delete
@@ -15,7 +30,6 @@ const TypeWriter = ( props ) => {
 	// setWriteArray & setAnimName to write
 	
 	// loop starting at step 2
-	
 	
 
 	return (
