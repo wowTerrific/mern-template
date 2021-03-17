2cc9b01926056aa8d8d2d1bd8d480ff62714e2ff
wowTerrific <marshallstutzman@gmail.com>
2021-03-17 14:26:26 -0400

    text change

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -50,14 +50,13 @@ const Services = () => {
 						
 						<br />
 						<br />
-						<h2 style={{color: "red", textAlign: "center"}}>**Gallery to go here**</h2>
+						<h2>Gallery Sample</h2>
 						<GalleryContainer>
 							<FlexContainer className="flex-align-items-center flex-justify-center">
 								<GalleryImage src="https://images.unsplash.com/photo-1615392030676-6c532fe0c302?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="space" className="flex-m-33" />
 								<GalleryImage src="https://images.unsplash.com/photo-1615377730068-e8235046ff05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="space" className="flex-m-33" />
 								<GalleryImage src="https://images.unsplash.com/photo-1615377730103-d12e4c5d26df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="space" className="flex-m-33" />
 							</FlexContainer>
-							<p>test</p>
 						</GalleryContainer>
 					</Container>
 				</section>
